16692efa59840b3094c3180129a8820b78c58931
Shehroz Ahmad <shehrozahmad970@gmail.com>
2021-12-22 21:28:11 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
